37a871845f4a2077b8c1e9546c95e7394f7ee61d
Vincen Wen <vincen.wen@samesamechina.com>
2015-05-06 16:55:37 +0800

    no message

--- js/wechat.js
+++ js/wechat.js
@@ -26,8 +26,8 @@ function wechatFun(){
 
 function wechatShare(timestamp_val,signature_val){
   wx.config({
-      debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-      appId: 'wxcc583f8544e50625', // 必填，公众号的唯一标识
+      debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+      appId: 'wx737a6d5fe4d19c89', // 必填，公众号的唯一标识
       timestamp: timestamp_val, // 必填，生成签名的时间戳
       nonceStr: 'asdkhaedhqwui', // 必填，生成签名的随机串
       signature: signature_val,// 必填，签名，见附录1
