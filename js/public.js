4215b93ad7327668bf55b9c017e2ba8f082ef3cf
Vincen Wen <vincen.wen@samesamechina.com>
2015-11-23 19:55:03 +0800

    no message

--- js/public.js
+++ js/public.js
@@ -127,12 +127,7 @@ ajaxfun("POST", "/Request.php?model=jssdk", jssdkPushData, "json", jssdkCallback
 
 function jssdkCallback(data){
     if(data.result == "success"){
-        alert(data.jsonResponse.timestamp);
-        alert(data.jsonResponse.appid);
-        alert(data.jsonResponse.noncestr);
-        alert(data.jsonResponse.signature);
-
-        wechatShare(data.appid,data.timestamp,data.noncestr,data.signature);
+        wechatShare(data.jsonResponse.appid,data.jsonResponse.timestamp,data.jsonResponse.nonceStr,data.jsonResponse.signature);
     }else{
         //console.log(data.msg);
     }
@@ -149,13 +144,13 @@ var shareData = {
             imgUrl: 'http://' + window.location.host + '/imgs/share.jpg'
 };
 
-function wechatShare(timestamp_val,signature_val){
+function wechatShare(appid_val,timestamp_val,nonceStr_val,signature_val){
 
   wx.config({
       debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-      appId: 'wx737a6d5fe4d19c89', // 必填，公众号的唯一标识
+      appId: appid_val, // 必填，公众号的唯一标识
       timestamp: timestamp_val, // 必填，生成签名的时间戳
-      nonceStr: 'asdkhaedhqwui', // 必填，生成签名的随机串
+      nonceStr: nonceStr_val, // 必填，生成签名的随机串
       signature: signature_val,// 必填，签名，见附录1
       jsApiList: [
         'checkJsApi',
