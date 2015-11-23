ae55a8a39cb59a4f3e395b51bd488d5e4bbb7c69
Vincen Wen <vincen.wen@samesamechina.com>
2015-11-23 14:21:51 +0800

    no message

--- js/index.js
+++ js/index.js
@@ -267,6 +267,7 @@ function myVideo(_this){
 
         $(".loading").hide();
         var video = document.createElement("VIDEO");
+        video.setAttribute("id", "video");
         video.setAttribute("width", "100%");
         video.setAttribute("height", "100%");
         video.setAttribute("controls", "controls");
@@ -274,6 +275,16 @@ function myVideo(_this){
         video.setAttribute("autoplay", "autoplay");
         video.setAttribute("src", vurl);
         document.body.appendChild(video);
+
+        launchFullscreen(document.getElementById('video')); 
+        window.setTimeout(function exit(){
+    //檢查瀏覽器是否處於全屏
+            if(invokeFieldOrMethod(document,'FullScreen') || invokeFieldOrMethod(document,'IsFullScreen') || document.IsFullScreen)
+            {
+            exitFullscreen();
+            }
+       },5*1000);
+
            
     } , function (p){
         //console.log(p+"%");
@@ -284,6 +295,106 @@ function myVideo(_this){
     return false;
     //document.getElementById("demo").innerHTML = "<b>注释：</b>IE 和 Safari 不支持 .ogg 文件格式。这只是一个例子。如需使其在所有浏览器中运行，您应该在 video 元素中使用 source 元素。";
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//反射調用
+var invokeFieldOrMethod = function(element, method) 
+{
+   var usablePrefixMethod;
+   ["webkit", "moz", "ms", "o", ""].forEach(function(prefix) {
+       if (usablePrefixMethod) return;
+       if (prefix === "") {
+           // 无前缀，方法首字母小写
+           method = method.slice(0,1).toLowerCase() + method.slice(1);   
+       }
+       var typePrefixMethod = typeof element[prefix + method];
+       if (typePrefixMethod + "" !== "undefined") {
+           if (typePrefixMethod === "function") {
+               usablePrefixMethod = element[prefix + method]();
+           } else {
+               usablePrefixMethod = element[prefix + method];
+           }
+       }
+   });
+    
+       return usablePrefixMethod;
+};
+    
+//進入全屏
+ function launchFullscreen(element) 
+   {
+    //此方法不可以在異步任務中執行，否則火狐無法全屏
+     if(element.requestFullscreen) {
+       element.requestFullscreen();
+     } else if(element.mozRequestFullScreen) {
+       element.mozRequestFullScreen();
+     } else if(element.msRequestFullscreen){ 
+       element.msRequestFullscreen();  
+     } else if(element.oRequestFullscreen){
+        element.oRequestFullscreen();
+    }
+    else if(element.webkitRequestFullscreen)
+     {
+       element.webkitRequestFullScreen();
+     }else{
+      
+        var docHtml  = document.documentElement;
+        var docBody  = document.body;
+        var videobox  = document.getElementById('videobox');
+        var  cssText = 'width:100%;height:100%;overflow:hidden;';
+        docHtml.style.cssText = cssText;
+        docBody.style.cssText = cssText;
+        videobox.style.cssText = cssText+';'+'margin:0px;padding:0px;';
+        document.IsFullScreen = true;
+ 
+      }
+   }
+//退出全屏
+   function exitFullscreen()
+   {
+       if (document.exitFullscreen) {
+         document.exitFullscreen();
+       } else if (document.msExitFullscreen) {
+         document.msExitFullscreen();
+       } else if (document.mozCancelFullScreen) {
+         document.mozCancelFullScreen();
+       } else if(document.oRequestFullscreen){
+            document.oCancelFullScreen();
+        }else if (document.webkitExitFullscreen){
+         document.webkitExitFullscreen();
+       }else{
+        var docHtml  = document.documentElement;
+        var docBody  = document.body;
+        var videobox  = document.getElementById('videobox');
+        docHtml.style.cssText = "";
+        docBody.style.cssText = "";
+        videobox.style.cssText = "";
+        document.IsFullScreen = false;
+    }
+  }
+
+
 
 
 
