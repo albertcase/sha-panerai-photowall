8c4a2d2a8965f782185c3f168e9f98448323d8c7
Vincen Wen <vincen.wen@samesamechina.com>
2015-11-23 19:56:05 +0800

    no message

--- js/public.js
+++ js/public.js
@@ -127,7 +127,7 @@ ajaxfun("POST", "/Request.php?model=jssdk", jssdkPushData, "json", jssdkCallback
 
 function jssdkCallback(data){
     if(data.result == "success"){
-        wechatShare(data.jsonResponse.appid,data.jsonResponse.timestamp,data.jsonResponse.nonceStr,data.jsonResponse.signature);
+        wechatShare(data.jsonResponse.appid, data.jsonResponse.timestamp, data.jsonResponse.nonceStr, data.jsonResponse.signature);
     }else{
         //console.log(data.msg);
     }
@@ -144,7 +144,7 @@ var shareData = {
             imgUrl: 'http://' + window.location.host + '/imgs/share.jpg'
 };
 
-function wechatShare(appid_val,timestamp_val,nonceStr_val,signature_val){
+function wechatShare(appid_val, timestamp_val, nonceStr_val, signature_val){
 
   wx.config({
       debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
